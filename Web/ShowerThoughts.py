a64835a4de7c8e9323e73305da12b91e3f6debaf
Leah Sandacz <sandas@outlook.com>
2017-09-19 11:52:48 -0700

    Ask user for count

--- Web/ShowerThoughts.py
+++ Web/ShowerThoughts.py
@@ -1,10 +1,21 @@
 import requests
 import json
 
-# Gets 5 'hot' submissions from showerthoughts reddit using the Requests package
+# Gets 'hot' submissions from showerthoughts reddit using the Requests package
 
-numThoughts = 10
+# Ask user for count
+inputNumThoughts = input("Number of thoughts (1-100): ")
 
+try:
+    int(inputNumThoughts)
+except ValueError:
+    print("Invalid number: {}".format(inputNumThoughts))
+    exit(1)
+
+numThoughts = int(inputNumThoughts)
+numThoughts = max(1, min(100, numThoughts))
+
+# Get listings from http request
 r = requests.get('https://www.reddit.com/r/showerthoughts/hot.json?limit={}'.format(numThoughts), headers = {'User-agent': 'showerbotpyscript 1.0'})
 
 if r.status_code != 200:
@@ -12,5 +23,6 @@ if r.status_code != 200:
 
 data = json.loads(r.text)
 
+# Print listings
 for i in range(0, numThoughts):
     print(str(i+1) + '. ' + data["data"]["children"][i]["data"]["title"])
