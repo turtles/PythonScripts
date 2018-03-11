8d1de7daa38deb78089d1031e7301b852df9b2fb
Leah Sandacz <sandas@outlook.com>
2018-03-10 19:50:44 -0800

    Daily coding problem

---
+++ DailyCodingProblem/march10.py
@@ -0,0 +1,21 @@
+# Given an array of integers, find the first missing positive integer
+# in linear time and constant space. In other words, find the lowest
+# positive integer that does not exist in the array. The array can
+# contain duplicates and negative numbers as well.
+# For example, the input [3, 4, -1, 1] should give 2.
+# The input [1, 2, 0] should give 3.
+
+def firstMissingPosInt(arr):
+    val = 0
+    changed = True
+    while changed:
+        changed = False
+        for i in range(0,len(arr)):
+            if arr[i]==val:
+                val += 1
+                changed = True
+
+    return val
+
+
+print(firstMissingPosInt([1,2,0]))
